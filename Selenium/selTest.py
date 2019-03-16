b672192b11a159cd717459198624a7c7f785edfa
danwaterfield <36337888+danwaterfield@users.noreply.github.com>
2019-03-16 17:14:24 +0000

    more failure

--- Selenium/selTest.py
+++ Selenium/selTest.py
@@ -1,8 +1,13 @@
-import selenium
-from selenium.webdriver import chrome
-from selenium.webdriver.chrome.options import Options
-opts = Options()
-opts.set_headless()
-assert opts.headless  # Operating in headless mode
-browser = chrome(opts)
-browser.get('https://duckduckgo.com')
+import time
+from selenium import webdriver
+
+# currently broken, trying to figure out the path to the chromedriver executable. grrgrgrgrgrg
+
+driver = webdriver.Chrome('~/danwaterfield/documents/github/webscraping/Selenium/ChromeDriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
